12a86f6ae1aba227e084671ae16bdf01a2afdfa4
Dinniatul <kundinnii29@gmail.com>
2022-10-28 14:42:44 +0700

    add component landingPages

--- public/server/index.js
+++ public/server/index.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const path = require("path");
+// const express = require("express");
+// const path = require("path");
 
-const app = express();
+// const app = express();
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-// routing
+// // routing
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/index.html"));
+// });
 
-app.get("/cars", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/cariMobil.html"));
-});
+// app.get("/cars", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../public/cariMobil.html"));
+// });
 
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
 
-app.listen(8000, "0.0.0.0", () => {
-  console.log("Server sudah berjalan, silahkan buka http://0.0.0.0:%d", 8000);
-});
+// app.listen(8000, "0.0.0.0", () => {
+//   console.log("Server sudah berjalan, silahkan buka http://0.0.0.0:%d", 8000);
+// });
 
-app.get("*", function (req, res) {
-  res.send("Halaman Tidak ditemukan!", 404);
-});
+// app.get("*", function (req, res) {
+//   res.send("Halaman Tidak ditemukan!", 404);
+// });
