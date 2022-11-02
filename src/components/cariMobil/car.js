adf4d9fc0b7257ffce0e697a1cb5b1751baa9fb5
Dinniatul <kundinnii29@gmail.com>
2022-11-03 00:40:19 +0700

    fix error code

--- src/components/cariMobil/car.js
+++ src/components/cariMobil/car.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 const Car = () => {
   const [carData, setCarData] = useState([]);
   const [displayCar, setDisplayCars] = useState([]);
-  //const [populateCars, setPopulateCars] = useState([]);
   const [driverType, setDriverType] = useState("");
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
@@ -14,6 +13,7 @@ const Car = () => {
       .then((response) => response.json())
       .then((data) => {
         console.log("data", data);
+
         setCarData(data);
       })
       .catch((err) => {
@@ -21,70 +21,41 @@ const Car = () => {
       });
   }, []);
 
-  // //membuat variabel baru menggabungkan waktu dan tanggal
-  // const newDateTime = new Date(`${date} ${time}`);
-  // const epochTime = newDateTime.getTime();
-
-  // class Binar {
-  //   static populateCars = (cars) => {
-  //     return cars.map((car) => {
-  //       const isPositive = getRandomInt(0, 1) === 1;
-  //       const timeAt = new Date();
-  //       const mutator = getRandomInt(1000000, 100000000);
-  //       const availableAt = new Date(timeAt.getTime() + (isPositive ? mutator : -1 * mutator));
-
-  //       return {
-  //         ...car,
-  //         availableAt,
-  //       };
-  //     });
-  //   };
-
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+  const populateCars = carData.map((item) => {
+    const isPositive = getRandomInt(0, 1) === 1;
+    const timeAt = new Date();
+    const mutator = getRandomInt(1000000, 100000000);
+    const availableAt = new Date(timeAt.getTime() + (isPositive ? mutator : -1 * mutator));
 
-  // function populateCars() {
-  //   {
-  //     carData.map((item) => {
-  //       const isPositive = getRandomInt(0, 1) === 1;
-  //       const timeAt = new Date();
-  //       const mutator = getRandomInt(1000000, 100000000);
-  //       const availableAt = new Date(timeAt.getTime() + (isPositive ? mutator : -1 * mutator));
-
-  //       return {
-  //         ...item,
-  //         availableAt,
-  //       };
-  //     });
-  //   }
-  // }
-
-  function populateCars() {
-    displayCar.map((item) => {
-      const isPositive = getRandomInt(0, 1) === 1;
-      const timeAt = new Date();
-      const mutator = getRandomInt(1000000, 100000000);
-      const availableAt = new Date(timeAt.getTime() + (isPositive ? mutator : -1 * mutator));
-
-      //setPopulateCars(populateCars);
-      return {
-        ...item,
-        availableAt,
-      };
-    });
-  }
+    return {
+      ...item,
+      availableAt,
+    };
+  });
 
   const handleSearchCar = () => {
-    console.log(driverType, date, time, capacity, populateCars);
+    const today = new Date();
+    const newDateTime = new Date(`${date} ${time}`);
+    console.log(today, newDateTime);
+    if (driverType === "") {
+      alert("Please select driver type");
+      return;
+    } else if (!date) {
+      alert("Please select date");
+      return;
+    } else if (newDateTime < today) {
+      alert("Dont select past time");
+      return;
+    }
 
-    const filteredCars = populateCars.filter((item) => (item.available === true) && (item.capacity >= setCapacity) && (item.availableAt >= setDate));
-
-    console.log("filteredCars", filteredCars);
-
-    setDisplayCars(filteredCars);
+    const filterCars = populateCars.filter((item) => item.capacity >= capacity && item.availableAt >= newDateTime);
+    console.log("filteredCars", filterCars);
+    setDisplayCars(filterCars);
   };
 
   return (
@@ -129,12 +100,6 @@ const Car = () => {
                     </svg>
                     Cari
                   </button>
-                  {/* <button type="button" className="btn btn-success my-4  " id="hapus-btn">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-                      <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
-                    </svg>
-                    Hapus{" "}
-                  </button> */}
                 </div>
               </div>
             </div>
@@ -143,53 +108,59 @@ const Car = () => {
       </section>
 
       <div className=" container row mt-5 mx-auto ">
-        {displayCar.map((item) => (
-          <div key={item.id} className="col-lg-4 d-flex mt-5">
-            <div className="card ">
-              <div className="card-body">
-                <div className="image-card ">
-                  <center>
-                    {" "}
-                    <img src={item.image} className="img-card" />
-                  </center>
+        {displayCar.length == 0 ? (
+          <center>
+            <h1 className="mt-5 mb-5">Data Mobil Tidak Ada</h1>
+          </center>
+        ) : (
+          displayCar.map((item) => (
+            <div key={item.id} className="col-lg-4 d-flex mt-5">
+              <div className="card ">
+                <div className="card-body">
+                  <div className="image-card ">
+                    <center>
+                      {" "}
+                      <img src={item.image} className="img-card" />
+                    </center>
+                  </div>
+                  <p className="mt-4">
+                    {item.manufacture}/{item.type}
+                  </p>
+                  <h5 className="fw-bold">
+                    {new Intl.NumberFormat("id-ID", {
+                      style: "currency",
+                      currency: "IDR",
+                    }).format(item.rentPerDay)}{" "}
+                    / hari
+                  </h5>
+                  <p className="mt-0">{item.description}</p>
+                  <p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-people" viewBox="0 0 16 16">
+                      <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
+                    </svg>{" "}
+                    {item.capacity} Orang
+                  </p>
+                  <p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-gear" viewBox="0 0 16 16">
+                      <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+                      <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+                    </svg>{" "}
+                    {item.transmission}
+                  </p>
+                  <p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-calendar4" viewBox="0 0 16 16">
+                      <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1H2zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5z" />
+                    </svg>{" "}
+                    Tahun {item.year}
+                  </p>
+                  <button href="#" className="btn btn-success text-white w-100 mt-2 fw-bold mt-4 " style={{ fontSize: "14px" }}>
+                    Pilih Mobil
+                  </button>
                 </div>
-                <p className="mt-4">
-                  {item.manufacture}/{item.type}
-                </p>
-                <h5 className="fw-bold">
-                  {new Intl.NumberFormat("id-ID", {
-                    style: "currency",
-                    currency: "IDR",
-                  }).format(item.rentPerDay)}{" "}
-                  / hari
-                </h5>
-                <p className="mt-0">{item.description}</p>
-                <p>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-people" viewBox="0 0 16 16">
-                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
-                  </svg>{" "}
-                  {item.capacity} Orang
-                </p>
-                <p>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-gear" viewBox="0 0 16 16">
-                    <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
-                    <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
-                  </svg>{" "}
-                  {item.transmission}
-                </p>
-                <p>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-calendar4" viewBox="0 0 16 16">
-                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1H2zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5z" />
-                  </svg>{" "}
-                  Tahun {item.year}
-                </p>
-                <button href="#" className="btn btn-success text-white w-100 mt-2 fw-bold mt-4 " style={{ fontSize: "14px" }}>
-                  Pilih Mobil
-                </button>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
